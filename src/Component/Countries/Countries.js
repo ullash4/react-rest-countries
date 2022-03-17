11ae549e2f0c30d83d970d03f78c26424a0d5b2f
ullash4 <programmerullash@gmail.com>
2022-03-17 09:54:41 +0600

    component file

---
+++ src/Component/Countries/Countries.js
@@ -0,0 +1,13 @@
+import './Countries.css'
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Hello from Countries component</h1>
+        </div>
+    );
+};
+
+export default Countries;
